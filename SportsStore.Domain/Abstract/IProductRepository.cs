f09d697fe70d5e285c3a156d7106c1e76994d97e
John Hampton <john_j_hampton@yahoo.com>
2018-06-14 03:30:14 -0400

    Create abstract repository

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
